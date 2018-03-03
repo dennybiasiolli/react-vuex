b64d9d75fe6821f8c1c9589c4e5ca188cf5db455
Denny Biasiolli <denny.biasiolli@gmail.com>
2018-03-03 15:07:52 +0100

    improving sources structure

---
+++ src/components/Provider.jsx
@@ -0,0 +1,26 @@
+import { STORE_KEY } from '../constants'
+
+export class Provider extends React.Component {
+  getChildContext() {
+    return {
+      [STORE_KEY]: this[STORE_KEY],
+    }
+  }
+
+  constructor(props, context) {
+    super(props, context)
+    this[STORE_KEY] = props.store;
+  }
+
+  render() {
+    return React.Children.only(this.props.children)
+  }
+}
+
+Provider.propTypes = {
+  store: PropTypes.object.isRequired,
+  children: PropTypes.element.isRequired,
+}
+Provider.childContextTypes = {
+  [STORE_KEY]: PropTypes.object.isRequired,
+}
