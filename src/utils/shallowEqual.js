c014268614ee2b09d149f358964a8d5fd7c2404a
Denny Biasiolli <denny.biasiolli@gmail.com>
2018-03-25 22:19:35 +0200

    tests: improving shallowEqual tests covering more options

--- src/utils/shallowEqual.js
+++ src/utils/shallowEqual.js
@@ -1,3 +1,8 @@
+/**
+ * Thanks to react-redux
+ * https://github.com/reactjs/react-redux/blob/master/src/utils/shallowEqual.js
+ */
+
 const hasOwn = Object.prototype.hasOwnProperty;
 
 const is = (x, y) => {
@@ -8,23 +13,24 @@ const is = (x, y) => {
 };
 
 export default (objA, objB) => {
-  if (is(objA, objB)) {
-    return true;
-  }
+  if (is(objA, objB)) return true;
+
   if (typeof objA !== 'object' || objA === null ||
     typeof objB !== 'object' || objB === null) {
     return false;
   }
+
   const keysA = Object.keys(objA);
   const keysB = Object.keys(objB);
-  if (keysA.length !== keysB.length) {
-    return false;
-  }
+
+  if (keysA.length !== keysB.length) return false;
+
   for (let i = 0; i < keysA.length; i += 1) {
     if (!hasOwn.call(objB, keysA[i]) ||
       !is(objA[keysA[i]], objB[keysA[i]])) {
       return false;
     }
   }
+
   return true;
 };
