b57f9e7b834229880c73bf0285de33e0946dbe21
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-03-18 11:10:07 +0100

    upgrading rollup related dependencies

--- build/rollup/rollup.test.config.js
+++ build/rollup/rollup.test.config.js
@@ -1,7 +1,7 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import replace from 'rollup-plugin-replace';
-import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
 import { uglify } from 'rollup-plugin-uglify';
 
 const env = process.env.NODE_ENV;
@@ -22,10 +22,12 @@ const config = {
   plugins: [
     nodeResolve(),
     babel({
+      babelHelpers: 'bundled',
       exclude: '**/node_modules/**',
     }),
     replace({
       'process.env.NODE_ENV': JSON.stringify(env),
+      preventAssignment: true,
     }),
     commonjs(),
   ],
