6cac925952c40c1d75efbeea4fb426eaca7e70ff
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-03-20 18:08:28 +0100

    updating rollup config to use rollup-plugin-terser

--- build/rollup/rollup.config.js
+++ build/rollup/rollup.config.js
@@ -2,7 +2,7 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
-// import { uglify } from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
 
 const env = process.env.NODE_ENV;
 
@@ -32,14 +32,8 @@ const config = {
   ],
 };
 
-// if (env === 'production') {
-//   config.plugins.push(uglify({
-//     compress: {
-//       pure_getters: true,
-//       unsafe: true,
-//       unsafe_comps: true,
-//     },
-//   }));
-// }
+if (env === 'production') {
+  config.plugins.push(terser());
+}
 
 export default config;
